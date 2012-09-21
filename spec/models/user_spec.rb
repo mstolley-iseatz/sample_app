5ddf72db60449e256b5b41d0c0b75853822dc1cf
Matthew Stolley <mstolley@iseatz.com>
2012-09-21 17:02:57 -0500

    Sign in / Sign out capabilities

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,8 +13,10 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com",
-  													password: "foobar", password_confirmation: "foobar") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -23,18 +25,19 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
   describe "when name is not present" do
-  	before { @user.name = " " }
-  	it { should_not be_valid }
+    before { @user.name = " " }
+    it { should_not be_valid }
   end
 
   describe "when name is too long" do
-  	before { @user.name = "a" * 51 }
-  	it { should_not be_valid }
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
@@ -102,6 +105,11 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
